d0bbaaf82e1a931408252004216fa7a42ad59687
Randold8 <84187343+Randold8@users.noreply.github.com>
2022-10-20 23:33:47 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,24 @@
+def toBase(num,base):
+    num = int(num)
+    base = int(base)
+    ans = ""
+    while num>0:
+        ans += str(num%base)
+        num //= base
+    return ans[::-1]
+cond = True
+while cond == True:
+    number = (input('Введите число '))
+    if number.isnumeric():
+        cond = False
+    else:
+        print('Такое нельзя!')
+while cond == False:
+    base = input('В какую систему счисления переводим? ')
+    if base.isnumeric():
+        cond = True
+    else:
+        print('В такую нельзя!')
+print(toBase(number,base))
+        
+    
